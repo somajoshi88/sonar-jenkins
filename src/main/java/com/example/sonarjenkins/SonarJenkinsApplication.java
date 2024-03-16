6b46a9b2d14dcb31399829c9c8ab2c5a67deabe2
somajoshi <somacse.joshi@gmail.com>
2024-03-16 12:01:35 +0530

    first commit

---
+++ src/main/java/com/example/sonarjenkins/SonarJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonarjenkins/SonarJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
